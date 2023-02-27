00c2e7781c179a96e8d5d7656c5ee571faa3da26
BronzePotato <BronzeVulture101@gmail.com>
2023-02-27 12:19:51 -0800

    Update PlayerController.cs

--- PlayerControlHinojosaP4/Assets/Scripts/PlayerController.cs
+++ PlayerControlHinojosaP4/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     private float speed = 12.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
@@ -18,12 +21,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We'll move the vehicle forward
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        // Moves the car forward based on vertical input
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //Rotates the car based on horizontal input
+
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
